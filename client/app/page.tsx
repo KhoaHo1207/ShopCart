abd7a97d6c1e3d3048431a1a80624fc89a3ba926
Thomas Ho <khoaho120703@gmail.com>
2025-12-03 15:31:54 +0700

    [client] feat: Footer Component UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="p-10">
+      <h2>Home page</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa
+        temporibus aut consequatur officiis natus, sequi rerum laudantium illo
+        quos expedita aspernatur laboriosam distinctio molestias quo libero a
+        suscipit veniam quas!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+import Container from "./Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container className="bg-red-300 w-full">Header</Container>
+    </header>
+  );
+};
+
+export default Header;
