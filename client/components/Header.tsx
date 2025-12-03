01392e513d9e1cad49dfdcf910f797151899ab49
Thomas Ho <khoaho120703@gmail.com>
2025-12-03 16:23:49 +0700

    [client] feat: Navbar, Sidebar ui component

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,9 +1,28 @@
+import CartIcon from "./CartIcon";
 import Container from "./Container";
+import FavoriteButton from "./FavoriteButton";
+import HeaderMenu from "./HeaderMenu";
+import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
+import SearchBar from "./SearchBar";
+import SignIn from "./SignIn";
 
 const Header = () => {
   return (
-    <header>
-      <Container className="bg-red-300 w-full">Header</Container>
+    <header className="bg-white py-5 border-b border-b-black/20">
+      <Container className="flex items-center justify-between">
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+          <MobileMenu />
+          <Logo />
+        </div>
+        <HeaderMenu />
+        <div className="flex flex-row justify-end items-center w-auto md:w-1/3 gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavoriteButton />
+          <SignIn />
+        </div>
+      </Container>
     </header>
   );
 };
